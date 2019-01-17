7aafa0d3f3c2b8105b183870604c6103eb7f628e
shichanglin <1052606486@qq.com>
2019-01-17 17:12:51 +0800

    自行车

--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,30 @@
 var iNow = 0;
+
 var last = -1;
-$(document).swipeUp(function(){
-    last= iNow;
+
+$(document).swipeUp(function () {
+    last = iNow;
     iNow++;
+    if (iNow <= $(".page").length - 1) {
+
+    }
     $(".page").eq(last).addClass("moveToTop");
     $(".page").eq(iNow).removeClass("hide").addClass("moveFromBottom")
-
-    setTimeout(function(){
+    setTimeout(function () {
         $(".page").eq(iNow).find("img").removeClass("hide");
-    },600)
+    }, 600)
+})
+$(document).swipeDown(function () {
+    if (iNow >= 1) {
+        last = iNow;
+        iNow--;
+        $(".page").eq(last).addClass("moveToTop");
+        $(".page").eq(iNow).removeClass("moveFromBottom").addClass("hide")
+        setTimeout(function () {
+            $(".page").eq(iNow).find("img").removeClass("hide");
+        }, 600)
+    }
+    else {
+        return 0;
+    }
 })
