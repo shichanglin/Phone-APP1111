e5bcd178f9c044753d3546f3902b4302cb8a19f5
shichanglin <1052606486@qq.com>
2019-01-18 16:35:20 +0800

    制造

--- demo/index.js
+++ demo/index.js
@@ -5,26 +5,60 @@ var last = -1;
 $(document).swipeUp(function () {
     last = iNow;
     iNow++;
-    if (iNow <= $(".page").length - 1) {
+    if (iNow <= $('.page').length - 1) {
+        $('.page').eq(last).attr('class', 'page page-' + (last + 1) + ' moveToTop');
+        $('.page').eq(iNow).attr("class", 'page page-' + (iNow + 1) + ' moveFromBottom');
 
+        setTimeout(function () {
+            $('.page').eq(iNow).find('img').removeClass('hide');
+            $('.page').eq(last).find('img').addClass('hide');
+        }, 600);
     }
-    $(".page").eq(last).addClass("moveToTop");
-    $(".page").eq(iNow).removeClass("hide").addClass("moveFromBottom")
-    setTimeout(function () {
-        $(".page").eq(iNow).find("img").removeClass("hide");
-    }, 600)
+
 })
 $(document).swipeDown(function () {
     if (iNow >= 1) {
         last = iNow;
         iNow--;
-        $(".page").eq(last).addClass("moveToTop");
-        $(".page").eq(iNow).removeClass("moveFromBottom").addClass("hide")
+        $('.page').eq(last).attr('class', 'page page-' + (last + 1) + ' moveToBottom');
+        $('.page').eq(iNow).attr('class', 'page page-' + (iNow + 1) + ' moveFromTop');
         setTimeout(function () {
-            $(".page").eq(iNow).find("img").removeClass("hide");
-        }, 600)
+            $('.page').eq(iNow).find('img').removeClass('hide');
+            $('.page').eq(last).find('img').addClass('hide');
+        }, 600);
     }
-    else {
-        return 0;
-    }
+
 })
+// 当前的  划出来
+// var iNow = 0;
+// // 划出去
+// var last = -1;
+
+// $(document).swipeUp(function () {
+//     last = iNow;
+//     iNow++;
+//     if (iNow <= $('.page').length - 1) {
+//         $('.page').eq(last).attr('class', 'page page-' + (last + 1) + ' moveToTop');
+//         $('.page').eq(iNow).removeClass('hide').attr('class', 'page page-' + (iNow + 1) + ' moveFromBottom');
+
+//         setTimeout(function () {
+//             // 等iNow 当前页 600ms划出来后  再让图片出来开始执行动画
+//             $('.page').eq(iNow).find('img').removeClass('hide');
+//             $('.page').eq(last).find('img').addClass('hide');
+//         }, 600);
+//     }
+// })
+// $(document).swipeDown(function () {
+//     if (iNow >= 1) {
+//         last = iNow;
+//         iNow--;
+//         $('.page').eq(last).attr('class', 'page page-' + (last + 1) + ' moveToBottom');
+//         $('.page').eq(iNow).attr('class', 'page page-' + (iNow + 1) + ' moveFromTop');
+//         setTimeout(function () {
+//             // 等iNow 当前页 600ms划出来后  再让图片出来开始执行动画
+//             $('.page').eq(iNow).find('img').removeClass('hide');
+//             $('.page').eq(last).find('img').addClass('hide');
+//         }, 600);
+//     }
+// })
+
